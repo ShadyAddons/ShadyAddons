d7ea0b595f26b40836668b72ee75d169a21f1f2d
Jerome Paulos <jeromepaulos@yahoo.com>
2021-09-27 17:17:30 -0700

    Set version during compile

--- src/main/java/cheaters/get/banned/utils/HttpUtils.java
+++ src/main/java/cheaters/get/banned/utils/HttpUtils.java
@@ -5,7 +5,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import cheaters.get.banned.Shady;
 
 public class HttpUtils {
 
@@ -13,7 +12,7 @@ public class HttpUtils {
      * Modified from Danker's Skyblock Mod under GPL 3.0 license
      * https://github.com/bowser0000/SkyblockMod/blob/master/LICENSE
      */
-    public static CloseableHttpClient client = HttpClients.custom().setUserAgent("ShadyAddons/"+Shady.VERSION).addInterceptorFirst((HttpRequestInterceptor) (request, context) -> {
+    public static CloseableHttpClient client = HttpClients.custom().setUserAgent("ShadyAddons/@VERSION@").addInterceptorFirst((HttpRequestInterceptor) (request, context) -> {
         if (!request.containsHeader("Pragma")) request.addHeader("Pragma", "no-cache");
         if (!request.containsHeader("Cache-Control")) request.addHeader("Cache-Control", "no-cache");
     }).build();
