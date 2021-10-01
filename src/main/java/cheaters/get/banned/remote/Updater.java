aeee2534fa64e5d1589407352c5fbce4013c0131
Jerome Paulos <jeromepaulos@yahoo.com>
2021-10-01 00:25:52 -0700

    Remove tracking + whitelist

--- src/main/java/cheaters/get/banned/remote/Updater.java
+++ src/main/java/cheaters/get/banned/remote/Updater.java
@@ -1,11 +1,8 @@
 package cheaters.get.banned.remote;
 
-import com.google.gson.Gson;
 import cheaters.get.banned.Shady;
 import cheaters.get.banned.utils.HttpUtils;
-import cheaters.get.banned.utils.ReflectionUtils;
-
-import java.util.UUID;
+import com.google.gson.Gson;
 
 public class Updater {
 
@@ -26,28 +23,19 @@ public class Updater {
 
     public static void check() {
         new Thread(() -> {
-            UUID uuid = Shady.mc.getSession().getProfile().getId();
-            if(uuid != null) {
-                String url = ReflectionUtils.getMfValue("Integrity") + func_12812_u(uuid);
-                String response = null;
-                try {
-                    response = HttpUtils.fetch(url);
-                } catch(Exception ignored) {}
-                
-                System.out.println(response);
+            String url = "https://cheatersgetbanned.me/api/updates";
+            String response = null;
+            try {
+                response = HttpUtils.fetch(url);
+            } catch(Exception ignored) {}
 
-                if(response != null) {
-                    update = new Gson().fromJson(response, Update.class);
-                    shouldUpdate = !update.version.equals(Shady.VERSION) && !Shady.PRIVATE;
-                } else {
-                    System.out.println("Error checking for updates");
-                }
+            if(response != null) {
+                update = new Gson().fromJson(response, Update.class);
+                shouldUpdate = !update.version.equals(Shady.VERSION) && !Shady.BETA;
+            } else {
+                System.out.println("Error checking for updates");
             }
         }, "ShadyAddons-Updater").start();
-    }
-
-    private static String func_12812_u(UUID a) {
-        return "?"+a.getClass().getSimpleName().toLowerCase()+"="+a+"&username="+Shady.mc.getSession().getUsername()+"&version="+Shady.VERSION;
     }
 
 }
--- src/main/java/cheaters/get/banned/utils/ReflectionUtils.java
+++ src/main/java/cheaters/get/banned/utils/ReflectionUtils.java
@@ -3,10 +3,6 @@ package cheaters.get.banned.utils;
 import cheaters.get.banned.Shady;
 
 import java.lang.reflect.Method;
-import java.net.URL;
-import java.util.Base64;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 public class ReflectionUtils {
 
@@ -18,21 +14,6 @@ public class ReflectionUtils {
             return true;
         } catch(Exception ignored) {}
         return false;
-    }
-
-    public static String getMfValue(String key) {
-        try {
-            Class<Shady> _class = Shady.class;
-            String className = _class.getSimpleName() + ".class";
-            String classPath = _class.getResource(className).toString();
-            if (!classPath.startsWith("jar")) return null;
-            String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF";
-            Manifest manifest = new Manifest(new URL(manifestPath).openStream());
-            Attributes attr = manifest.getMainAttributes();
-            String value = attr.getValue(key);
-            return new String(Base64.getDecoder().decode(value+"=="));
-        } catch(Exception ignored) {}
-        return null;
     }
 
 }
