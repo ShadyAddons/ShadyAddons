a8b1207f71d96f53fb9b354204e5cb34d344acba
Jerome Paulos <jeromepaulos@yahoo.com>
2021-12-05 22:55:36 -0800

    Added heartbeat analytics

--- src/main/java/cheaters/get/banned/Shady.java
+++ src/main/java/cheaters/get/banned/Shady.java
@@ -43,6 +43,8 @@ import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -144,6 +146,10 @@ public class Shady {
         USING_SBE = Loader.isModLoaded("skyblockextras");
 
         Analytics.collect("hash", CrashReporter.hashMod());
+
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+            Analytics.collect("heartbeat", String.valueOf(System.currentTimeMillis()));
+        }, 5, 5, TimeUnit.MINUTES);
     }
 
     @SubscribeEvent
