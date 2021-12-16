4dea1dfc1351f2fce865be395bb7f2069fa7f63f
Jerome Paulos <jeromepaulos@yahoo.com>
2021-12-15 18:38:22 -0800

    Unsatisfactory mod ID hider

--- src/main/java/cheaters/get/banned/mixins/MixinModList.java
+++ src/main/java/cheaters/get/banned/mixins/MixinModList.java
@@ -2,18 +2,24 @@ package cheaters.get.banned.mixins;
 
 import cheaters.get.banned.Shady;
 import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.network.handshake.FMLHandshakeMessage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.Map;
 
-// @Mixin(FMLHandshakeMessage.ModList.class)
+@Mixin(FMLHandshakeMessage.ModList.class)
 public abstract class MixinModList {
 
-    // @ModifyArg(method = "<init>(Ljava/util/List;)V", at = @At("INVOKE"))
-    private static List<ModContainer> modifyModList(List<ModContainer> modList) {
-        System.out.println("Before: "+modList);
-        modList.removeIf(mod -> mod.getName().equals(Shady.MOD_NAME));
-        System.out.println("After: "+modList);
-        return modList;
+    @Shadow private Map<String, String> modTags;
+
+    @Inject(method = "<init>(Ljava/util/List;)V", at = @At("RETURN"), remap = false)
+    private void removeMod(List<ModContainer> modList, CallbackInfo ci) {
+        modTags.remove(Shady.MOD_ID);
     }
 
 }
