9e368b34f598f0ae44dbf7b6216d5627a3e4cbe0
Tadd <unconfigured@null.spigotmc.org>
2022-05-06 13:58:51 +0200

    Changed Blaze Daggers Swap to work more consistently

--- src/main/java/cheaters/get/banned/features/AutoBlazeSword.java
+++ src/main/java/cheaters/get/banned/features/AutoBlazeSword.java
@@ -1,16 +1,23 @@
 package cheaters.get.banned.features;
 
 import cheaters.get.banned.Shady;
+import cheaters.get.banned.events.ClickEvent;
 import cheaters.get.banned.gui.config.Config;
 import cheaters.get.banned.utils.KeybindUtils;
 import cheaters.get.banned.utils.LocationUtils;
 import cheaters.get.banned.utils.Utils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AutoBlazeSword {
 
@@ -30,22 +37,39 @@ public class AutoBlazeSword {
         ash.add("HEARTFIRE_DAGGER");
     }
 
+    public boolean checkSkeleton(Entity entity){
+        if(entity instanceof EntitySkeleton){
+            if(((EntitySkeleton)entity).getSkeletonType() == 1){
+                return true;
+            }
+        }
+        return false;
+    }
 
-    //TODO:Better detection of attunement
     @SubscribeEvent
-    public void onChat(ClientChatReceivedEvent event){
+    public void onLeftClick(ClickEvent.Left event){
         if(!enabled) enable();
         if(!LocationUtils.onIsland(LocationUtils.Island.CRIMSON_ISLE) || !Config.autoBlazeDagger) return;
-        if(event.message.getUnformattedText().contains("attunement")){
-            String blazeAttunement = event.message.getUnformattedText().split(" ")[3];
-            int daggerAsh = -1, daggerSpirit = -1;
+        Entity pointed = Shady.mc.pointedEntity;
+        if(pointed instanceof EntityBlaze || pointed instanceof EntityPigZombie || checkSkeleton(pointed)){
+            int daggerAsh = -1, daggerSpirit = -1, attunement = 0;
+            List<Entity> entities = Shady.mc.theWorld.getEntitiesWithinAABBExcludingEntity(pointed, pointed.getEntityBoundingBox().expand(0.5, 0.5, 0.5)).stream().filter(entity -> entity instanceof EntityArmorStand).collect(Collectors.toList());
+            for(Entity e : entities) {
+                String name = e.getDisplayName().getUnformattedText().toLowerCase();
+                if(name.contains("ashen")) attunement = 1;
+                else if(name.contains("auric")) attunement = 2;
+                else if(name.contains("spirit")) attunement = 3;
+                else if(name.contains("crystal")) attunement = 4;
+            }
             for(int i=0; i<9; i++) {
                 ItemStack item = Shady.mc.thePlayer.inventory.getStackInSlot(i);
                 if(spirit.contains(Utils.getSkyBlockID(item))) daggerSpirit = i;
                 else if(ash.contains(Utils.getSkyBlockID(item))) daggerAsh = i;
             }
-            switch(blazeAttunement) {
-                case "ASHEN":
+            switch(attunement) {
+                case 0:
+                    break;
+                case 1:
                     if (daggerAsh != -1){
                         if (daggerAsh != Shady.mc.thePlayer.inventory.currentItem)
                             Shady.mc.thePlayer.inventory.currentItem = daggerAsh;
@@ -53,7 +77,7 @@ public class AutoBlazeSword {
                             KeybindUtils.rightClick();
                     }
                     break;
-                case"AURIC":
+                case 2:
                     if (daggerAsh != -1) {
                         if (daggerAsh != Shady.mc.thePlayer.inventory.currentItem)
                             Shady.mc.thePlayer.inventory.currentItem = daggerAsh;
@@ -61,7 +85,7 @@ public class AutoBlazeSword {
                             KeybindUtils.rightClick();
                     }
                     break;
-                case"SPIRIT":
+                case 3:
                     if (daggerSpirit != -1) {
                         if (daggerSpirit != Shady.mc.thePlayer.inventory.currentItem)
                             Shady.mc.thePlayer.inventory.currentItem = daggerSpirit;
@@ -69,7 +93,7 @@ public class AutoBlazeSword {
                             KeybindUtils.rightClick();
                     }
                     break;
-                case"CRYSTAL":
+                case 4:
                     if (daggerSpirit != -1) {
                         if (daggerSpirit != Shady.mc.thePlayer.inventory.currentItem)
                             Shady.mc.thePlayer.inventory.currentItem = daggerSpirit;
