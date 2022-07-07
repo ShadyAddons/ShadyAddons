e25f128b2b7a1693cae5a47fa49edccd74d5b0a9
5dt1 <58152963+5dt1-CDA@users.noreply.github.com>
2022-07-07 13:16:19 +0200

    Update AutoTerminals for new terminals

--- src/main/java/cheaters/get/banned/features/autoterminals/NewAutoTerminals.java
+++ src/main/java/cheaters/get/banned/features/autoterminals/NewAutoTerminals.java
@@ -3,20 +3,27 @@ package cheaters.get.banned.features.autoterminals;
 import cheaters.get.banned.features.autoterminals.solvers.*;
 import cheaters.get.banned.gui.config.Config;
 import cheaters.get.banned.utils.Utils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NewAutoTerminals {
 
     private static long lastClick = 0;
     public static int windowClicks = 0;
-
+    
+    
     @SubscribeEvent
     public void onGuiDraw(GuiScreenEvent.BackgroundDrawnEvent event) {
         if(!Config.autoTerminals || !Utils.inDungeon || !(event.gui instanceof GuiChest)) return;
@@ -42,18 +49,25 @@ public class NewAutoTerminals {
             clickQueue = new ColorSolver().getClicks(slots, chestName);
         } else if(chestName.equals("Change all to same color!")) {
             clickQueue = new ToggleColorSolver().getClicks(slots, chestName);
+        } else if(chestName.equals("Click the button on time!")) {
+        	clickQueue = new TimingSolver().getClicks(slots, chestName);
         } else {
             return;
         }
 
         if(clickQueue == null) {
-            Utils.log("Something is wrong with the click queue");
+            Utils.log("Something is possibly wrong with the click queue");
             return;
         }
-
-        if(System.currentTimeMillis() - lastClick >= Config.terminalClickDelay) {
-            windowClicks = clickQueue.clickFirst(windowClicks);
-            lastClick = System.currentTimeMillis();
+        if(clickQueue.size() != 0) {
+	        if(System.currentTimeMillis() - lastClick >= Config.terminalClickDelay) {
+	            windowClicks = clickQueue.clickFirst(windowClicks);
+	            lastClick = System.currentTimeMillis();
+	            if(clickQueue.size() == 0) {
+	            	 ToggleColorSolver.resetColorNeeded();
+	            }
+	        }
+	        
         }
     }
 
